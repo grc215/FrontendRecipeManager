4a57e46fe626403452142d307328951991da0029
Greg <caldwell.gregoryj@gmail.com>
2021-04-19 20:03:32 -0400

    I created the bottom list and added an event listener such that if any are clicked they replace the top recipe title, image, and ingredients. Also moved h3, image, and ingredientLi to global.

--- index.js
+++ index.js
@@ -4,65 +4,45 @@ let divMain = document.createElement('div')
   divMain.className = ('main-display')
   body.append(divMain)
 
+let h3 = document.createElement('h3')
+let image = document.createElement('img')
+let ingredientLi = document.createElement('li')
+
+let divBottom = document.createElement('div')
+  divBottom.className = ('bottom-display')
+  body.append(divBottom)
+
 
 fetch('http://localhost:3000/recipes/1')
 .then(resp => resp.json())
 .then(Obj => {
   console.log(Obj)
-  let h3 = document.createElement('h3')
+  
     h3.innerText = Obj.name
-  let image = document.createElement('img')
     image.src = Obj.image
-  let blankLi = document.createElement('li')
-    blankLi.innerText = Obj.ingredients
+    ingredientLi.innerText = Obj.ingredients
 
-
-    divMain.append(h3, image, blankLi)
+    divMain.append(h3, image, ingredientLi)
   // let p = document.createElement('p')
   //   p.innerText = 
-
 })
 
+fetch('http://localhost:3000/recipes')
+  .then(res => res.json())
+  .then((recipeArr) => {
+    recipeArr.forEach((recipeObj) => {
+      let nameLi = document.createElement('li')
+      nameLi.innerText = recipeObj.name
 
+      divBottom.append(nameLi)
 
+      //Event Listener in the same level  
+      nameLi.addEventListener("click", (evt) => {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let blankUl = document.createElement('ul')
-//   blankUl.className = 'main-recipe'
-//   div.append(blankUl)
+        h3.innerText = recipeObj.name
+        image.src = recipeObj.image
+        ingredientLi.innerText = recipeObj.ingredients
+        
+    })
+    })
+  })
